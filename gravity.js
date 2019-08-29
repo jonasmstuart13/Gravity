52ecc48683f8291112deb117fe23badd33461bfe
jomostu13 <jonasmstuart13@gmail.com>
2019-08-29 21:06:23 +0700

    Adds initialVectorFunction

---
+++ gravity.js
@@ -0,0 +1,50 @@
+const accelerationDueToGravity = -9.8;
+var initVelocity = 0;
+var initAngle = 0;
+const initUpwardVelocity = Math.sin(initAngle);
+const initHorizontalVelocity = Math.cos(initAngle);
+var initHeight = 0;
+
+function updateVelocity(num) {
+  initVelocity = num;
+  initHorizontalVelocity = Math.cos(initAngle);
+  initUpwardVelocity = Math.sin(initAngle);
+}
+
+function updateHeight(h) {
+  initHeight = h;
+}
+
+function horizontalFunction(t) {
+  return initHorizontalVelocity * t;
+}
+
+function timeByDistanceFunction(d) {
+  return (1 / initHorizontalVelocity) * d;
+}
+
+function verticalFunction(t) {
+  return (
+    (accelerationDueToGravity / 2) * t * t + initUpwardVelocity * t + initHeight
+  );
+}
+
+function heightToDistance(d) {
+  return verticalFunction(timeByDistanceFunction(d));
+}
+
+function giveCurrentUpwardVelocity(t) {
+  return -9.8 * t + initUpwardVelocity;
+}
+
+function giveCurrentHorizontalVelocity() {
+  return initHorizontalVelocity;
+}
+
+function giveCurrentVelocity(t) {
+  return Math.pow(
+    Math.pow(giveCurrentUpwardVelocity(t), 2) +
+      Math.pow(giveCurrentHorizontalVelocity(), 2),
+    0.5
+  );
+}
