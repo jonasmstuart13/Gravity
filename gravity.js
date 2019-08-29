47deeaa6564aa5c2882b206d695868e34902357f
jonasmstuart13 <54625051+jonasmstuart13@users.noreply.github.com>
2019-08-29 21:09:21 +0700

    Update gravity.js

--- gravity.js
+++ gravity.js
@@ -33,22 +33,3 @@ function heightToDistance(d) {
   return verticalFunction(timeByDistanceFunction(d));
 }
 
-function giveCurrentUpwardVelocity(t) {
-  return -9.8 * t + initUpwardVelocity;
-}
-
-function giveCurrentHorizontalVelocity() {
-  return initHorizontalVelocity;
-}
-
-function giveCurrentVelocity(t) {
-  return Math.pow(
-    Math.pow(giveCurrentUpwardVelocity(t), 2) +
-      Math.pow(giveCurrentHorizontalVelocity(), 2),
-    0.5
-  );
-}
-
-function giveInitialVelocityVector() {
-  return [Math.cos(initAngle), Math.sin(initAngle)];
-}
