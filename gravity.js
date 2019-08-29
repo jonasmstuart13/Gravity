29e0f173ac4e821ba0657a5fa7c9358c6235e807
jonasmstuart13 <54625051+jonasmstuart13@users.noreply.github.com>
2019-08-29 21:48:20 +0700

    Update gravity.js

--- gravity.js
+++ gravity.js
@@ -1,35 +1,62 @@
-const accelerationDueToGravity = -9.8;
-var initVelocity = 0;
-var initAngle = 0;
-const initUpwardVelocity = Math.sin(initAngle);
-const initHorizontalVelocity = Math.cos(initAngle);
-var initHeight = 0;
+var GravityFunctions = {
+  accelerationDueToGravity: -9.8,
+  initVelocity: 0,
+  initAngle: 0,
+  initUpwardVelocity: Math.sin(this.initAngle) * this.initVelocity,
+  initHorizontalVelocity: Math.cos(this.initAngle) * this.initVelocity,
+  initHeight: 0,
+  askInitializeInfo() {
+    this.initAngle = parseFloat(prompt("Intial Angle: "));
+    this.initvelocity = parseFloat(prompt("Initial Velocity: "));
+    this.initHeight = parseFloat(prompt("Initial Height: "));
+    this.setHorizontalAndVerticalVelocity();
+  },
+  setInitVelocity(v) {
+    this.initVelocity = v;
+  },
+  setInitAngle(a) {
+    this.initAngle = a;
+  },
+  setInitHeight(h) {
+    this.initHeight = h;
+  },
+  setHorizontalAndVerticalVelocity(a) {
+    this.initUpwardVelocity = Math.sin(this.initAngle) * this.initVelocity;
+    this.initHorizontalVelocity = Math.cos(this.initAngle) * this.initVelocity;
+  },
+  horizontalFunction(t) {
+    return this.initHorizontalVelocity * t;
+  },
+  timeByDistanceFunction(d) {
+    return (1 / this.initHorizontalVelocity) * d;
+  },
+  verticalFunction(t) {
+    return (
+      (this.accelerationDueToGravity / 2) * t * t +
+      this.initUpwardVelocity * t +
+      this.initHeight
+    );
+  },
+  heightToDistance(d) {
+    return verticalFunction(timeByDistanceFunction(d));
+  },
+  giveCurrentUpwardVelocity(t) {
+    return -9.8 * t + this.initUpwardVelocity;
+  },
+  giveCurrentHorizontalVelocity() {
+    return this.initHorizontalVelocity;
+  },
+  giveCurrentVelocity(t) {
+    return Math.pow(
+      Math.pow(giveCurrentUpwardVelocity(t), 2) +
+        Math.pow(giveCurrentHorizontalVelocity(), 2),
+      0.5
+    );
+  },
+  giveInitialVelocityVector() {
+    return [Math.cos(this.initAngle), Math.sin(this.initAngle)];
+  }
+};
 
-function updateVelocity(num) {
-  initVelocity = num;
-  initHorizontalVelocity = Math.cos(initAngle);
-  initUpwardVelocity = Math.sin(initAngle);
-}
-
-function updateHeight(h) {
-  initHeight = h;
-}
-
-function horizontalFunction(t) {
-  return initHorizontalVelocity * t;
-}
-
-function timeByDistanceFunction(d) {
-  return (1 / initHorizontalVelocity) * d;
-}
-
-function verticalFunction(t) {
-  return (
-    (accelerationDueToGravity / 2) * t * t + initUpwardVelocity * t + initHeight
-  );
-}
-
-function heightToDistance(d) {
-  return verticalFunction(timeByDistanceFunction(d));
-}
+  
 
